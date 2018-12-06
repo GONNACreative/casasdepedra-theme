81d436a4e6d0a653ca41396b9e36d8989db8336b
Tony de Faria <tonydefaria@iigethr.com>
2018-12-06 21:12:16 +0000

    small details

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 // Main
 
 bindEvents = function() {
+  $("body").hide();
   $(".altai-center").altaiCenter();
   $(".altai-off").altaiOff();
   $(".altai-scroll").altaiScroll();
   $(".altai-toggle").altaiToggle();
+  $("body").delay(250).fadeIn(500);
+  $("a.gallery").featherlightGallery({
+		previousIcon: "&#9664;",
+		nextIcon: "&#9654;",
+		galleryFadeIn: 100,
+		galleryFadeOut: 300
+	});
 }
 $( document ).ready(function() {
   bindEvents();
